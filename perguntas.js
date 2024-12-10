f5da7c2cbaf2db5fbb416007bd1069d764103f38
LaraGhiraldi <00001075480218sp@al.educacao.sp.gov.br>
2024-12-10 17:31:40 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Sobre mim',
+    'Qual minha comida favorita?',
+    'Hamburguer e pizza'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Sobre mim',
+    'Qual minha cor preferida?',
+    'Verde'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Sobre mim',
+    'Qual minha música favorita?',
+    'Iris - The Goo Goo Dolls'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Sobre mim',
+    'Qual meu livro preferido?',
+    'A Quarta Asa'
 )
